047965d9c5be6cae10de80473579639d1c8eeec2
Maylida289 <4519210082@univpancasila.ac.id>
2024-01-22 18:59:33 +0700

    add configuration

--- config/config.go
+++ config/config.go
@@ -1,1 +1,17 @@
 package config
+
+import "github.com/tkanos/gonfig"
+
+type Config struct {
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_HOST     string
+	DB_PORT     string
+	DB_NAME     string
+}
+
+func GetConfig() Config {
+	result := Config{}
+	gonfig.GetConf("config/config.json", &result)
+	return result
+}
