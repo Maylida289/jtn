7ba9dec957c1452dd193b167e4b349a81d265207
Maylida289 <4519210082@univpancasila.ac.id>
2024-01-23 23:03:16 +0700

    add database configuration

---
+++ database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"fmt"
+	"jtn/config"
+
+	// "gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+var err error
+
+func Init() {
+	conf := config.GetConfig()
+
+	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", conf.DB_USERNAME, conf.DB_PASSWORD, conf.DB_HOST, conf.DB_PORT, conf.DB_NAME))
+
+	if err != nil {
+		panic("database error")
+	}
+
+	err = db.DB().Ping()
+
+	if err != nil {
+		panic("DSN Error")
+	}
+}
