577400391b6ab47bba8d839f4d37bd673289c0ac
Maylida289 <4519210082@univpancasila.ac.id>
2024-01-25 07:58:23 +0700

    add validator

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"jtn/database"
 	"jtn/routes"
+
+	"github.com/go-playground/validator/v10"
 )
+
+type CustomValidator struct {
+	Validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.Validator.Struct(i)
+}
 
 func main() {
 	database.Init()
 	e := routes.Init()
+	e.Validator = &CustomValidator{
+		Validator: validator.New(),
+	}
 
-	e.Logger.Fatal(e.Start("localhost:8002"))
+	e.Logger.Fatal(e.Start(":8002"))
 }
